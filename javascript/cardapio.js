d96c2cb79b50926575ca232b6c5603c5c64c1036
elderlima7 <114250931+elderlima7@users.noreply.github.com>
2023-06-13 14:12:40 -0300

    continuação cardapio

--- javascript/cardapio.js
+++ javascript/cardapio.js
@@ -2,9 +2,9 @@ const button01 = document.getElementById('button-entrada');
 const button02 = document.getElementById('button-americanbbq');
 const button03 = document.getElementById('button-parrilla');
 
-const box01 = document.getElementById('box-option-01');
-const box02 = document.getElementById('box-option-02');
-const box03 = document.getElementById('box-option-03');
+const box01 = document.getElementById('box-option-comidas-01');
+const box02 = document.getElementById('box-option-comidas-02');
+const box03 = document.getElementById('box-option-comidas-03');
 
 button01.addEventListener('click', function() {
     box01.classList.remove('hidden');

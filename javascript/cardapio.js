7e008f6e2e21403b1461087193796e429c133181
Sophizada <santosvsophia17@gmail.com>
2023-06-16 14:34:42 -0300

    cardapio 87% feito

---
+++ javascript/cardapio.js
@@ -0,0 +1,37 @@
+const button01 = document.getElementById('button-entrada');
+const button02 = document.getElementById('button-americanbbq');
+const button03 = document.getElementById('button-parrilla');
+
+const box01 = document.getElementById('box-option-comidas-01');
+const box02 = document.getElementById('box-option-comidas-02');
+const box03 = document.getElementById('box-option-comidas-03');
+
+button01.addEventListener('click', function() {
+    box01.classList.remove('hidden');
+    box02.classList.add('hidden');
+    box03.classList.add('hidden');
+
+    button01.classList.add('active');
+    button02.classList.remove('active');
+    button03.classList.remove('active');
+});
+
+button02.addEventListener('click', function() {
+    box01.classList.add('hidden');
+    box02.classList.remove('hidden');
+    box03.classList.add('hidden');
+
+    button01.classList.remove('active');
+    button02.classList.add('active');
+    button03.classList.remove('active');
+});
+
+button03.addEventListener('click', function() {
+    box01.classList.add('hidden');
+    box02.classList.add('hidden');
+    box03.classList.remove('hidden');
+
+    button01.classList.remove('active');
+    button02.classList.remove('active');
+    button03.classList.add('active');
+});
